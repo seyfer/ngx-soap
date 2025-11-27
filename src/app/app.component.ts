14df39a467550b748f2777ee59ad1bb5de7a189c
Oleg Abrazhaev <seyferseed@gmail.com>
2025-11-27 20:55:42 +0100

    feat: update angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { NgxSoapService, Client, ISoapMethodResponse } from '../../projects/ngx-soap/src/public_api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   title = 'SOAP Calculator app';
